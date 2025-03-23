6e830b9d489fa8d82dc13b1927695a695ec96d46
jeamxn <admin@chicken-moo.com>
2025-03-24 00:32:47 +0900

    fix: error handle

--- src/crons/2gik.ts
+++ src/crons/2gik.ts
@@ -6,54 +6,59 @@ import mongoose from "mongoose";
 import { dataDB } from "@/models/data";
 
 const run: CronConfig["run"] = async () => { 
-  const response = await axios.post(
-    "https://dorm2.khu.ac.kr/food/getWeeklyMenu.do",
-    new URLSearchParams({
-      "locgbn": "K1",
-      "sch_date": "",
-      "fo_gbn": "stu"
-    })
-  );
-  
-  const menus = response.data.root[0].WEEKLYMENU[0];
-  const datas = [];
-
-  for (let i = 1; i <= 7; i++) {
-    datas.push(
-      dataDB.updateOne(
-        { 
-          course: new mongoose.Types.ObjectId("67df8f651ebc907021326750"),
-          date: menus[`fo_date${i}`],
-        },
-        {
-          menu: menus[`fo_menu_mor${i}`].split(", "),
-        },
-        { upsert: true }
-      ),
-      dataDB.updateOne(
-        { 
-          course: new mongoose.Types.ObjectId("67df90831ebc907021326755"),
-          date: menus[`fo_date${i}`],
-        },
-        {
-          menu: menus[`fo_menu_lun${i}`].split(", "),
-        },
-        { upsert: true }
-      ),
-      dataDB.updateOne(
-        { 
-          course: new mongoose.Types.ObjectId("67df908b1ebc907021326756"),
-          date: menus[`fo_date${i}`],
-        },
-        {
-          menu: menus[`fo_menu_eve${i}`].split(", "),
-        },
-        { upsert: true }
-      ),
+  try {
+    const response = await axios.post(
+      "https://dorm2.khu.ac.kr/food/getWeeklyMenu.do",
+      new URLSearchParams({
+        "locgbn": "K1",
+        "sch_date": "",
+        "fo_gbn": "stu"
+      })
     );
+    
+    const menus = response.data.root[0].WEEKLYMENU[0];
+    const datas = [];
+  
+    for (let i = 1; i <= 7; i++) {
+      datas.push(
+        dataDB.updateOne(
+          { 
+            course: new mongoose.Types.ObjectId("67df8f651ebc907021326750"),
+            date: menus[`fo_date${i}`],
+          },
+          {
+            menu: menus[`fo_menu_mor${i}`].split(", "),
+          },
+          { upsert: true }
+        ),
+        dataDB.updateOne(
+          { 
+            course: new mongoose.Types.ObjectId("67df90831ebc907021326755"),
+            date: menus[`fo_date${i}`],
+          },
+          {
+            menu: menus[`fo_menu_lun${i}`].split(", "),
+          },
+          { upsert: true }
+        ),
+        dataDB.updateOne(
+          { 
+            course: new mongoose.Types.ObjectId("67df908b1ebc907021326756"),
+            date: menus[`fo_date${i}`],
+          },
+          {
+            menu: menus[`fo_menu_eve${i}`].split(", "),
+          },
+          { upsert: true }
+        ),
+      );
+    }
+    await Promise.all(datas);
+    console.log("💩 2gik done at:", dayjs().format("YYYY-MM-DD HH:mm:ss"));
   }
-  await Promise.all(datas);
-  console.log("💩 2gik done at:", dayjs().format("YYYY-MM-DD HH:mm:ss"));
+  catch {
+    console.error("💩 2gik error at:", dayjs().format("YYYY-MM-DD HH:mm:ss"));
+  }
 };
 
 const Cron_2Gik = cron({
